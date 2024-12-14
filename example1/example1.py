f7b46915cb95d2a26804c849a3f50d4db2d80450
PavelKovalev1444 <pavel.kovalev30@gmail.com>
2024-12-14 17:02:51 +0300

    MASTERDEGREE: feat add simply graph algo

---
+++ example1/example1.py
@@ -0,0 +1,12 @@
+with db.session() as session:
+        query = '''
+            LOAD CSV WITH HEADERS FROM "file:///home/pavel/repos/master-degree/app/tmp/nodes_movies.csv" AS row
+            CREATE (:Movie {
+                title: row.title,
+                Format: row.Format,
+                ReleaseYear: toInteger(row.ReleaseYear),
+                MPAA_Rating: row.MPAA_Rating,
+                Price: toFloat(row.Price)
+            })
+        '''
+        session.run(query)
