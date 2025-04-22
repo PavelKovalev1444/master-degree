c1e9ea1e4bf41035bada1c70c1dd0602e135b9cb
PavelKovalev1444 <pavel.kovalev30@gmail.com>
2025-04-23 02:10:10 +0300

    MASTERDEGREE: fixes for additional chapter

--- app/core/Core.py
+++ app/core/Core.py
@@ -9,14 +9,17 @@ SIMILARITY_LIMIT = 0.5
 class Core(object):
 
 
-    def __init__(self):
+    def __init__(self, verbose):
         self.serializer = DataSerializer()
+        self.verbose = verbose
         pass
 
 
     def load_movies(self, db, movies_df):
         iters = len(movies_df)
-        # iters = 20
+
+        if self.verbose:
+            print("Started creating movies nodes in db. Please, wait.")
 
         for i in range(iters):
             nodes_dict = movies_df.iloc[i].fillna(0).to_dict()
@@ -32,13 +35,17 @@ class Core(object):
                     })
                     SET n += node
                 """
-
                 session.run(query, nodes=nodes_dict)
+        
+        if self.verbose:
+            print("Finished creating movies nodes in db.")
     
     
     def load_directors(self, db, directors_df):
         iters = len(directors_df)
-        # iters = 20
+
+        if self.verbose:
+            print("Started creating directors nodes in db. Please, wait.")
 
         for i in range(iters):
             nodes_dict = directors_df.iloc[i].fillna(0).to_dict()
@@ -50,13 +57,17 @@ class Core(object):
                     })
                     SET n += node
                 """
-
                 session.run(query, nodes=nodes_dict)
+
+        if self.verbose:
+            print("Finished creating directors nodes in db.")
 
 
     def load_users(self, db, users_df):
         iters = len(users_df)
-        # iters = 20
+
+        if self.verbose:
+            print("Started creating users nodes in db. Please, wait.")
 
         for i in range(iters):
             nodes_dict = users_df.iloc[i].fillna(0).to_dict()
@@ -69,12 +80,17 @@ class Core(object):
                     })
                     SET n += node
                 """
-
                 session.run(query, nodes=nodes_dict)
+
+        if self.verbose:
+            print("Finished creating users nodes in db.")
 
 
     def load_genres(self, db, genres_df):
         iters = len(genres_df)
+
+        if self.verbose:
+            print("Started creating genres nodes in db. Please, wait.")
 
         for i in range(iters):
             nodes_dict = genres_df.iloc[i].fillna(0).to_dict()
@@ -86,8 +102,10 @@ class Core(object):
                     })
                     SET n += node
                 """
-
                 session.run(query, nodes=nodes_dict)
+
+        if self.verbose:
+            print("Finished creating genres nodes in db.")
 
 
     def load_csvs(self, db, movies_df, directors_df, users_df, genres_df):
@@ -99,7 +117,9 @@ class Core(object):
     
     def load_director_relations(self, db, relations_df):
         iters = len(relations_df)
-        # iters = 20
+
+        if self.verbose:
+            print("Started creating directors relations in db. Please, wait.")
 
         for i in range(iters):
             nodes_dict = relations_df.iloc[i].fillna(0).to_dict()
@@ -115,13 +135,17 @@ class Core(object):
                     CREATE (d)-[:DIRECTED]->(m)
                     RETURN m, d
                 """
-
                 session.run(query, nodes=nodes_dict)
+
+        if self.verbose:
+            print("Finished creating directors relations in db.")
 
     
     def load_user_relations(self, db, relations_df):
         iters = len(relations_df)
-        # iters = 20
+
+        if self.verbose:
+            print("Started creating users relations in db. Please, wait.")
 
         for i in range(iters):
             nodes_dict = relations_df.iloc[i].fillna(0).to_dict()
@@ -140,10 +164,15 @@ class Core(object):
 
                 session.run(query, nodes=nodes_dict)
 
+        if self.verbose:
+            print("Finished creating users relations in db.")
+
 
     def load_genre_relations(self, db, relations_df):
         iters = len(relations_df)
-        # iters = 20
+
+        if self.verbose:
+            print("Started creating genres relations in db. Please, wait.")
 
         for i in range(iters):
             nodes_dict = relations_df.iloc[i].fillna(0).to_dict()
@@ -155,8 +184,10 @@ class Core(object):
                     CREATE (m)-[:ASSOCIATED_WITH {accuracy: 1.0} ]->(g)
                     RETURN m, g
                 """
-
                 session.run(query, nodes=nodes_dict)
+
+        if self.verbose:
+            print("Finished creating genres relations in db.")
 
     
     def load_relations(self, db, director_relations_df, user_relations_df, genre_relations_df):
@@ -172,7 +203,6 @@ class Core(object):
         right_classifiers,
         right_features,
     ) -> float:
-        # Проверка размеров входных данных
         left_size = len(left_classifiers)
         right_size = len(right_classifiers)
         
@@ -185,22 +215,18 @@ class Core(object):
         if right_size != len(right_features):
             raise ValueError(f"Длина right_classifiers ({right_size}) должна быть равна длине right_features ({len(right_features)})")
 
-        # Создание объединения классификаторов
         union_classifier_set = set(left_classifiers)
         union_classifier_set.update(right_classifiers)
         union_classifiers = list(union_classifier_set)
 
-        # Получение векторов признаков
         left_vector = self.get_features_vector(left_classifiers, left_features, union_classifiers)
         right_vector = self.get_features_vector(right_classifiers, right_features, union_classifiers)
 
-        # Вычисление косинусного сходства
         result = self.cosine(left_vector, right_vector)
         return 0.0 if math.isnan(result) else result
 
 
     def cosine(self, vector_a, vector_b):
-        """Вычисляет косинусное сходство между двумя векторами."""
         dot_product = sum(a * b for a, b in zip(vector_a, vector_b))
         norm_a = math.sqrt(sum(x * x for x in vector_a))
         norm_b = math.sqrt(sum(x * x for x in vector_b))
@@ -213,7 +239,6 @@ class Core(object):
         classifier_features,
         union_classifiers
     ):
-        """Создает вектор признаков на основе классификаторов и их значений."""
         classifier_to_features = dict(zip(classifiers, classifier_features))
         return [
             classifier_to_features.get(classifier, 0.0)
@@ -223,6 +248,9 @@ class Core(object):
 
     def get_content_based_recommendation(self, db, recommendation_meta_info):
         user = recommendation_meta_info['user']
+
+        if self.verbose:
+            print("Started content-based filtering. Please, wait.")
 
         with db.session() as session:
             query = """
@@ -242,6 +270,9 @@ class Core(object):
             """
             result = session.run(query, user=user)
 
+            if self.verbose:
+                print("Finished fetching data from db. Calculating similarity.")
+
             movies = [self.serializer.serialize_content_based_raw(record) for record in result]
             processed_movies = []
             for movie in movies:
@@ -249,12 +280,17 @@ class Core(object):
                 if tmp_score > SIMILARITY_LIMIT:
                     processed_movies.append({'title': movie['title'], 'genres': movie['genres'], 'score': tmp_score})
             
+            if self.verbose:
+                print("Similarity has been calculated. Sorting results.")
+
             return sorted(processed_movies, key=lambda movie: movie['score'], reverse=True)
 
 
     def get_collaborative_filtering_recommendation(self, db, recommendation_meta_info):
-
         user = recommendation_meta_info['user']
+
+        if self.verbose:
+            print("Started collaborative filtering. Please, wait.")
 
         with db.session() as session:
             query="""
@@ -267,7 +303,6 @@ class Core(object):
                 WITH u AS user, similar, ux_classifiers, ux_features, COLLECT(classifier) AS classifiers, COLLECT(total_feature) AS features
                 RETURN user, similar, ux_classifiers, ux_features, classifiers, features
             """
-
             result = session.run(query, user=user)
             records = [self.serializer.serialize_collaborative_similar_users(record) for record in result]
             
@@ -300,7 +335,6 @@ class Core(object):
                     COLLECT(g.name) AS genres
                 RETURN title, ux_classifiers, ux_features, classifiers, features, genres
             """
-
             result = session.run(
                 query, 
                 user=user, 
@@ -309,11 +343,17 @@ class Core(object):
                 ux_features_list=[record['ux_features'] for record in processed_records]
             )
 
+            if self.verbose:
+                print("Finished fetching data from db. Calculating similarity.")
+
             movies = [self.serializer.serialize_collaborative(record) for record in result]
             processed_movies = []
             for movie in movies:
                 tmp_score = self.similar(movie['ux_classifiers'], movie['ux_features'], movie['classifiers'], movie['features'])
                 if tmp_score > SIMILARITY_LIMIT:
                     processed_movies.append({'title': movie['title'], 'genres': movie['genres'], 'score': tmp_score})
-            
-            return sorted(processed_movies, key=lambda movie: movie['score'], reverse=True)[0:5]
+
+            if self.verbose:
+                print("Similarity has been calculated. Sorting results.")
+
+            return sorted(processed_movies, key=lambda movie: movie['score'], reverse=True)
--- app/db/DatabaseCient.py
+++ app/db/DatabaseCient.py
@@ -6,14 +6,35 @@ from serializer.DataSerializer import DataSerializer
 class DatabaseClient(object):
 
 
-    def __init__(self):
+    def __init__(self, verbose):
+        self.verbose = verbose
+        self.db = None
         pass
 
 
     def connect_to_db(self):
-        self.db = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "test4test"))
+        try:
+            if self.verbose:
+                print("Trying to connect to db.")
+            self.db = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "test4test"))
+        except:
+            print("Cannot establish connection with db.")
+        else:
+            if self.verbose:
+                print("Has connected to db.")
+
         return self.db
 
 
     def close_connection(self):
-        self.db.close()
+        if self.verbose:
+            print("Trying to close db connection.")
+
+        try:
+            self.db.close()
+        except:
+            print("Cannot close connection to db.")
+        else:
+            if self.verbose:
+                print("Has closed conntection to db.")
+
